c876a016a4d8fed0c51a3abe35c7b33509a26a22
Cassie Kise <cassandrakise@gmail.com>
2022-10-24 16:56:33 -0700

    updated with completed rough of thought and user controllers

---
+++ controller/thoughtController.js
@@ -0,0 +1,67 @@
+const Thought = require('../models/thought');
+const User = require('../models/User');
+
+const thoughtController = {
+     getAllThoughts: async ( req, res ) =>  {
+        try {
+            const results = await Thought.find()
+            res.json(results)
+        }
+        catch(err) {
+            res.status(500).json(err);
+        }
+    },
+    getSingleThought: async ( req, res ) => {
+        try {
+            const results = await Thought.findOne({
+                _id: req.params.thoughtId })
+                .populate('friends')
+                .populate('thoughts')
+            res.json(results)
+        }
+        catch(err) {
+            res.status(500).json(err);
+        }
+    },
+    postNewThought: async (req, res) => {
+        try {
+            const results = await Thought.create(req.body)
+            const user = await User.findOneAndUpdate(
+                {_id: req.params.userId},
+                { $push: {
+                    thoughts: results._id
+                }}
+            )
+            res.json(results)                
+        }
+        catch(err) {
+            res.status(500).json(err);
+        }
+    },
+    updateThought: async (req, res) => {
+        try {
+            const results = await Thought.findOneAndUpdate( 
+                { _id: req.params.thoughtId},
+                { $set: req.body},
+                { runValidators: true, new: true}
+            ) 
+            res.json(results)
+        }
+        catch(err) {
+            res.status(500).json(err);
+        }
+    },
+    deleteThought: async (req, res) => {
+        try {
+            const results = await Thought.findOneAndDelete(
+                {_id: req.params.thoughtId},
+                res.json(results),
+            )
+        }
+        catch(err) {
+            res.status(500).json(err);
+        }
+    },
+}
+
+module.exports = thoughtController;
---
+++ controller/userController.js
@@ -0,0 +1,75 @@
+const User = require('../models/User');
+
+const userController = {
+     getAllUsers: async ( req, res ) =>  {
+        try {
+            const results = await User.find()
+            res.json(results)
+        }
+        catch(err) {
+            res.status(500).json(err);
+        }
+    },
+    getSingleUser: async ( req, res ) => {
+        try {
+            const results = await User.findOne({
+                _id: req.params.userId })
+                .populate('friends')
+                .populate('thoughts')
+            res.json(results)
+        }
+        catch(err) {
+            res.status(500).json(err);
+        }
+    },
+    postNewUser: async (req, res) => {
+        try {
+            const results = await User.create(req.body)
+            res.json(results)                
+        }
+        catch(err) {
+            res.status(500).json(err);
+        }
+    },
+    updateUser: async (req, res) => {
+        try {
+            const results = await User.findOneAndUpdate( 
+                { _id: req.params.userId},
+                { $set: req.body},
+                { runValidators: true, new: true}
+            ) 
+            res.json(results)
+        }
+        catch(err) {
+            res.status(500).json(err);
+        }
+    },
+    deleteUser: async (req, res) => {
+        try {
+            const results = await User.findOneAndDelete(
+                {_id: req.params.userId},
+                res.json(results),
+            )
+        }
+        catch(err) {
+            res.status(500).json(err);
+        }
+    },
+    addFriend: async (req, res) => {
+        try {
+            const results = await User.findOneAndUpdate(
+                { _id: req.params.userId},
+                { $addToSet: {
+                    friends: req.params.friendId
+                }},
+                { new: true }
+            )
+            res.json(results)                
+        }
+        catch(err) {
+            res.status(500).json(err);
+        }
+    }
+}
+
+module.exports = userController;
